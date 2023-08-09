57895192a5478840723f5d9e10b5dacfa591580e
Jid <raphjid170@gmail.com>
2023-08-09 14:38:45 +0300

    Some dependencies added plus code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!
+using System;
+using System.Collections;
+namespace ArrayRecap
+{
+    public class ArrayListDemo
+    {
+        public static void Main(){
+            ArrayList arrayList1 = new ArrayList();
+            arrayList1.Add("Jid");
+            arrayList1.Add(12);
+            arrayList1.Add(true);
+            arrayList1.Add("");
+            arrayList1.Add(4.5);
+        foreach (var item in arrayList1)
+        {
+            Console.WriteLine(item);
+        }
+        }
+    }
+}
